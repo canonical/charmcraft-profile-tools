22c70355d08c4e1f261240e25a1b39fcc999349e
Dave Wilding <david.wilding@canonical.com>
2025-12-12 15:33:57 +0800

    ci: fix workflows (#10)
    Fixes the following workflows:
    
    - **Current with Charmcraft** - This workflow builds Charmcraft from
    `main` and checks that `charmcraft init` generates the same charms as
    stored in this repo.
    
    The workflow was failing because `make setup` couldn't download
    https://security.ubuntu.com/ubuntu/pool/universe/p/python-apt/python-apt-dev_2.7.7ubuntu5_all.deb.
    After testing in a local VM, I found that I needed to run `sudo apt-get
    update` before `make setup`. This PR makes this addition to the
    workflow.
    
    - **Charm checks** - This workflow includes a check that the
    implementation script for kubernetes-extra produces the same charm as
    stored in the repo. The check was failing because some transitive
    dependencies of `requests` had had version bumps. For kubernetes-extra,
    we only care about primary dependencies added to `pyproject.toml`, so
    I've excluded `uv.lock` from the diff and specified an exact version of
    `requests`.

--- .implement/kubernetes-extra.py
+++ .implement/kubernetes-extra.py
@@ -27,7 +27,9 @@ def main():
     r.save()
 
     # Implement get_version() in the workload module, by requesting the version over HTTP.
-    subprocess.check_call(['uv', 'add', '--quiet', 'requests'])  # Add package to charm venv.
+    subprocess.check_call(
+        ['uv', 'add', '--quiet', 'requests==2.32.5']
+    )  # Add package to charm venv.
     r = rewriter.Rewriter('src/my_application.py')
     r.fwd('import logging')
     r.add('')
