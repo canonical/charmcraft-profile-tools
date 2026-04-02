ae46babf2bf3974fc8cf302a9e78b32108dd7851
David Wilding <david.wilding@canonical.com>
2026-04-02 14:45:12 +0800

    format script

--- inplace.py
+++ inplace.py
@@ -6,7 +6,6 @@ import subprocess
 
 OUR_CLONE = pathlib.Path() / "charmcraft-profile-tools" / "canonical-match-profiles"
 
-
 # Make sure we have a clone of the charmcraft-profile-tools repo, with a new branch checked out
 # in case we want to push the generated charms. Using https://github.com/dwilding/gimmegit.
 if not OUR_CLONE.exists:
