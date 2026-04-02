5504ff0e68ce77c6982f06a51947ae6283f41046
David Wilding <david.wilding@canonical.com>
2026-04-02 14:41:56 +0800

    add script

---
+++ inplace.py
@@ -0,0 +1,27 @@
+"""Run charmcraft-profile-tools from within the Charmcraft source."""
+
+import os
+import pathlib
+import subprocess
+
+OUR_CLONE = pathlib.Path() / "charmcraft-profile-tools" / "canonical-match-profiles"
+
+
+# Make sure we have a clone of the charmcraft-profile-tools repo, with a new branch checked out
+# in case we want to push the generated charms. Using https://github.com/dwilding/gimmegit.
+if not OUR_CLONE.exists:
+    subprocess.check_call(
+        [
+            "uvx",
+            "gimmegit",
+            "--allow-outer-repo",
+            "canonical/charmcraft-profile-tools",
+            "match-profiles",
+        ]
+    )
+
+# Generate charms and update the uv.lock templates in the Charmcraft source.
+env = os.environ.copy()
+env["CHARMCRAFT_DIR"] = str(pathlib.Path.cwd())
+subprocess.call(["uvx", "--from", "rust-just", "just", "init"], cwd=OUR_CLONE, env=env)
+subprocess.call(["uvx", "--from", "rust-just", "just", "lock"], cwd=OUR_CLONE, env=env)
